01eab746fceb060c7ac15a1570b819c198c1f021
Francisco V <vxrnxk@gmail.com>
2020-01-27 18:50:25 -0300

    Intro to classes

---
+++ lesson-2/index.php
@@ -0,0 +1,21 @@
+<?php
+
+class User {
+
+}
+
+$userOne = new User();
+$userTwo = new User();
+
+echo "The class is " . get_class($userTwo);
+
+?>
+
+<html>
+    <head>
+        <title>PHP OOP Tutorial</title>
+    </head>
+
+    <body>
+    </body>
+</html>
